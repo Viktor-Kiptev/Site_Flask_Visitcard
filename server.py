cefd170660388d6c0aec6e3e248b0cacee1487ac
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-21 17:22:37 +0100

    db

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -7,15 +8,39 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def htms_page(page_name):
     return render_template(page_name)
 
+
+# def write_to_file(data):
+#     with open('database.csv', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},\n{subject},\n{message}')
+
+def write_to_file_csv(data):
+    with open('database.csv', 'a', newline='') as database:
+        csv_writer = csv.writer(database, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    if request.method == 'POST'
-        data = request.form.to_dict()
-
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_file_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Didn\'t save to database'
+    else:
+        return 'Something went wrong. Try again'
 
 # @app.route('/index.html')
 # def main():
